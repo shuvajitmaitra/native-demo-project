22dbd3db00519f013256a84fceb485ed4d8c57c6
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-01-30 13:17:18 +0600

    Initital Project Setup

---
+++ app/(tabs)/profile/index.js
@@ -0,0 +1,24 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+   <>
+   <Stack.Screen options={{headerShown: true, title: "Profile"}} />
+   <View style={styles.container}>
+      <Text>Index profile Tab</Text>
+      <StatusBar style="auto" />
+    </View>
+   </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
