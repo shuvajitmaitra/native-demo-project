19b9093f445174a27b085b79354aa5bcfa4a3bbd
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-01-30 19:32:20 +0600

    tab routing added

--- app/(drawer)/_layout.js
+++ app/(drawer)/_layout.js
@@ -1,6 +1,38 @@
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text } from "react-native";
+import { router } from "expo-router";
 
+
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem icon={(color, size) => <AntDesign name="home" size={24} color="black" />}
+        label={"home"}
+        onPress={() => {
+          router.push('/(drawer)/(tabs)/home')
+        }}
+      />
+      <DrawerItem icon={(color, size) => <MaterialIcons name="contacts" size={24} color="black" />}
+        label={"Contact"}
+        onPress={() => {
+          router.push('/(drawer)/(tabs)/contact')
+        }}
+      />
+      <DrawerItem icon={(color, size) => <FontAwesome name="user-circle" size={24} color="black" />}
+        label={"Profile"}
+        onPress={() => {
+          router.push('/(drawer)/(tabs)/profile')
+        }}
+      />
+
+    </DrawerContentScrollView>
+  )
+}
 export default function _layout() {
-  return <Drawer />;
+  return <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />} />;
 }
